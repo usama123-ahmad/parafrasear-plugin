6e5aa4f413953c2dd02a4077a89ef8af6915d116
Muhamad Usama Ahmad <64584287+usama123-ahmad@users.noreply.github.com>
2023-06-01 15:58:55 +0500

    Second Commit

--- parafrasear.php
+++ parafrasear.php
@@ -35,9 +35,9 @@
         'all'
     );
 
-      wp_enqueue_script('script-parafrasear', $directory . 'js/main.js', array('wp-editor'), '1.0', true);
+      wp_enqueue_script('script-parafrasear', $directory . 'js/main.js', 'jQuery', '1.0.0', true);
       wp_localize_script('script-parafrasear', 'parafrasear_param', array(
-        'pluginsUrl' => plugins_url(),
+        'ajax_url' => admin_url('admin-ajax.php')
     ));
 
     }
@@ -70,3 +70,80 @@ function add_meta_box_parafrasear() {
     require plugin_dir_path( __FILE__ ). 'views/front.php';
 
   }
+
+
+
+
+  //Plugin Ajax Paraphrase Function
+function paraphrase_result_action() {
+  
+  global $wpdb;
+
+  $text =  $wpdb->_real_escape($_POST['text']);
+   
+  $text = sanitize_text_field($text);
+
+  $text = esc_html($text);
+
+             if(is_numeric($text))
+             {
+               echo json_encode(array(
+                 "error" => "Ingrese solo la cadena",
+                 "errortype" => "string"
+             ));
+               wp_die();
+             }
+            elseif (strlen($text) === 0)
+            {
+                echo json_encode(array(
+                    "error" => "Empty Input! Try entering some text in input box.",
+                    "errortype" => "empty-input"
+                ));
+                wp_die();
+            }
+            else
+            {
+                
+                $urlEncode = urlencode($text);
+                
+                $url = 'https://paraphrase-free.p.rapidapi.com/';
+                $body = array(
+                    'text' => $urlEncode,
+                    'mode' => 'm4',
+                    'dest' => 'es',
+                );
+                $headers = array(
+                    'X-RapidAPI-Host' => 'paraphrase-free.p.rapidapi.com',
+                    'X-RapidAPI-Key' => '7bc99aa18dmsh37f2c8ed91328c7p14442cjsn1f449b58b077',
+                    'content-type' => 'application/x-www-form-urlencoded',
+                );
+                
+                $response = wp_remote_post( $url, array(
+                    'method' => 'POST',
+                    'headers' => $headers,
+                    'body' => $body,
+                ) );
+
+
+
+                if ( is_wp_error( $response ) ) {
+                    $error_message = $response->get_error_message();
+                    // handle error
+                    echo wp_kses_data($error_message);
+                    wp_die();
+
+                } else {
+                    $response_body = wp_remote_retrieve_body( $response );
+                    // handle response
+
+                    echo wp_kses_data($response_body) ;
+                    wp_die();
+                }
+
+            }
+
+        
+    
+}
+add_action('wp_ajax_paraphrase_result_action', 'paraphrase_result_action');
+
